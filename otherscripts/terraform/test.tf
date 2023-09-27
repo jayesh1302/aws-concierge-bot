49e697a66b32d01253bfd357add5384eb818b828
Nick Chao <yc6371@nyu.edu>
2023-09-27 22:06:45 +0000

    add dynamodb test to terraform

--- otherscripts/terraform/test.tf
+++ otherscripts/terraform/test.tf
@@ -2,6 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "test_vpc" {
-  cidr_block = "10.0.0.0/16"
+locals {
+  json_data = file("test.json")
+  tf_data   = jsondecode(local.json_data)
+}
+
+resource "aws_dynamodb_table" "dynamodb_table" {
+  name         = "yelp-restaurants"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table_item" "dynamodb_table_item" {
+  for_each   = local.tf_data
+  table_name = aws_dynamodb_table.dynamodb_table.name
+  hash_key   = "id"
+  item       = jsonencode(each.value)
 }
