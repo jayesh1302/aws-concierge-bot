3d9cf1b7cc647ae4cae1d8c2ec885b544b95ba30
Nick Chao <yc6371@nyu.edu>
2023-10-07 01:12:45 -0400

    merge two tf files

--- otherscripts/terraform/main.tf
+++ otherscripts/terraform/main.tf
@@ -10,19 +10,10 @@ terraform {
 provider "aws" {
 }
 
-variable "sender_email" {}
-
-variable "es_host" {}
-
+/*-----------------SQS-----------------*/
 resource "aws_sqs_queue" "Q1_test" {
   name = "Q1-test"
 }
-
-output "Q1_test_url" {
-  description = "URL for SQS Q1 test"
-  value       = aws_sqs_queue.Q1_test.url
-}
-
 data "aws_iam_policy_document" "Q1_test_iam_policy" {
   statement {
     sid    = "First"
@@ -37,18 +28,19 @@ data "aws_iam_policy_document" "Q1_test_iam_policy" {
     resources = [aws_sqs_queue.Q1_test.arn]
   }
 }
-
 resource "aws_sqs_queue_policy" "test" {
   queue_url = aws_sqs_queue.Q1_test.id
   policy    = data.aws_iam_policy_document.Q1_test_iam_policy.json
 }
 
+/*-----------------LB2-----------------*/
+variable "sender_email" {}
+variable "es_host" {}
 data "archive_file" "python_lambda_package" {
   type        = "zip"
-  source_dir  = "${path.module}/package"
+  source_dir  = "${path.module}/lf2/package"
   output_path = "lf2.zip"
 }
-
 resource "aws_iam_role" "lambda_exec" {
   name = "lf2-role"
 
@@ -65,7 +57,6 @@ resource "aws_iam_role" "lambda_exec" {
     ]
   })
 }
-
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   for_each = toset([
     "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
@@ -78,7 +69,6 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = each.value
 }
-
 resource "aws_lambda_function" "lf2" {
   function_name = "LF2"
   filename      = "lf2.zip"
@@ -94,19 +84,17 @@ resource "aws_lambda_function" "lf2" {
   }
 }
 
-# CloudWatch trigger
+/*-----------------CloudWatch trigger-----------------*/
 resource "aws_cloudwatch_event_rule" "lf2_trigger" {
   name                = "lf2-trigger"
   description         = "Schedule lf2 function"
   schedule_expression = "rate(60 minutes)"
 }
-
 resource "aws_cloudwatch_event_target" "lf2_target" {
   target_id = "lf2-target"
   rule      = aws_cloudwatch_event_rule.lf2_trigger.name
   arn       = aws_lambda_function.lf2.arn
 }
-
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
@@ -115,7 +103,49 @@ resource "aws_lambda_permission" "allow_cloudwatch" {
   source_arn    = aws_cloudwatch_event_rule.lf2_trigger.arn
 }
 
-
+/*-----------------SES-----------------*/
 resource "aws_ses_email_identity" "example" {
   email = var.sender_email
+}
+
+/*-----------------DYNAMODB-----------------*/
+resource "aws_dynamodb_table" "dynamodb_table" {
+  name         = "yelp-restaurants"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+/*-----------------OPENSEARCH-----------------*/
+data "aws_caller_identity" "current" {}
+
+resource "aws_opensearch_domain" "opensearch" {
+  domain_name    = "restaurants"
+  engine_version = "Elasticsearch_7.10"
+
+  cluster_config {
+    instance_type = "t3.small.search"
+  }
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
+  access_policies = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "es:*",
+        "Principal": {
+          "AWS": ${aws_iam_role.lambda_exec.arn}
+        },
+        "Effect": "Allow",
+        "Resource": "arn:aws:es:us-east-1:${data.aws_caller_identity.current.account_id}:domain/restaurants/*",
+      }
+    ]
+  }
+  POLICY
 }
